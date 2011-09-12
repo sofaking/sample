958eddc938622c6c4ca27aae0ca0156be6c47e69
Anton Petrov <givemeletter@gmail.com>
2011-09-13 00:29:39 +0400

    Added heroku support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    if @title.nil?                                    # Boolean test for nil
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{@title}"                     # String interpolation
+    end
+  end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,10 +1,2 @@
 module PagesHelper
-  def title
-    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
-    if @title.nil?                                    # Boolean test for nil
-      base_title                                      # Implicit return
-    else
-      "#{base_title} | #{@title}"                     # String interpolation
-    end
-  end
 end
